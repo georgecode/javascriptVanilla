b255c49784bf66ad2d773de148b027771335365a
georgecode <georgeczurlanis@gmail.com>
2017-05-31 02:31:51 -0700

    Add example of prototype basics

---
+++ ScopeObjectsPrototypeClasses/PrototypeBacics.js
@@ -0,0 +1,28 @@
+function talk(){
+	
+	console.log(this.sound)
+}
+
+let animal ={
+	talk:talk
+}
+
+animal.talk()//undefined
+
+//This is the es6way you only need to add the property it knows the value
+
+// let animal ={
+// 	talk
+// }
+
+let cat ={
+	sound:'meow!'
+}
+
+//this is where we set the cat prototype to animal
+//this basicially means javascript will look for a property
+//in the cat object first but if it dosent have one it will go
+//up the chain to the animal object
+Object.setPrototypeOf(cat, animal)
+
+cat.talk()//meow!
