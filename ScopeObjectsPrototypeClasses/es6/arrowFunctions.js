b1f43275655a783cf8908395980b48d0eae2dffb
georgecode <georgeczurlanis@gmail.com>
2017-06-03 02:38:18 -0700

    Add more arrow functions

--- ScopeObjectsPrototypeClasses/es6/arrowFunctions.js
+++ ScopeObjectsPrototypeClasses/es6/arrowFunctions.js
@@ -1,4 +1,6 @@
+//https://www.sitepoint.com/es6-arrow-functions-new-fat-concise-syntax-javascript/
 
+/////////////////////////////////////////////////////////
 //Basic function 
 
 //es5
@@ -9,8 +11,11 @@ var func5 = function(){
 //es6
 let func6 = () => {return "ES6 function"};
 
-console.log(func5())
-console.log(func6())
+console.log(func5())//Es5 function
+console.log(func6())//ES6 function
+
+
+
 
 /////////////////////////////////////////////////////////
 
@@ -24,10 +29,8 @@ var funcParam5 = function(name){
 //es6
 let funcParam6 = (name) => name.split("");
 
-console.log(funcParam5('bob'))
-console.log(funcParam6('mary'))
-
-
+console.log(funcParam5('bob'))//[ 'b', 'o', 'b' ]
+console.log(funcParam6('mary'))//[ 'm', 'a', 'r', 'y' ]
 
 
 
@@ -44,8 +47,11 @@ var funcParams5 = function(x,y){
 //es6
 let funcParams6 = (x,y) => {return x+y};
 
-console.log(funcParams5(5,5))
-console.log(funcParams6(5,5))
+console.log(funcParams5(5,5))//10
+console.log(funcParams6(5,5))//10
+
+
+
 
 /////////////////////////////////////////////////////////
 
@@ -66,10 +72,83 @@ const saysBark6 = (state) =>({
 	})
 
 
-saysBark5({name:' kona'}).bark()
-saysBark6({name:' bonus'}).bark()
+saysBark5({name:' kona'}).bark()//Woff, I am kona
+saysBark6({name:' bonus'}).bark()//Woff, I am bonus
+
+
+
 
 /////////////////////////////////////////////////////////
+
+// .map
+
+var smartPhones = [
+	{ name:'iphone', price:649 },
+	{ name:'Galaxy S6', price:576 },
+	{ name:'Galaxy Note 5', price:489 }
+];
+
+
+//es5
+console.log(smartPhones.map(
+    function(smartPhone) {
+	return smartPhone.price;
+    }
+));//[ 649, 576, 489 ]
+
+//es6
+console.log(smartPhones.map(
+	smartPhone=>smartPhone.price
+));//[ 649, 576, 489 ]
+
+
+
+
+/////////////////////////////////////////////////////////
+
+// .filter
+
+var array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16];
+
+//es5
+var odd5 = array.filter(function(num){
+	return num % 2;
+})
+
+
+var odd6 = array.filter(num => num % 2);
+
+
+console.log(odd5)//[ 1, 3, 5, 7, 9, 11, 13, 15 ]
+console.log(odd6)//[ 1, 3, 5, 7, 9, 11, 13, 15 ]
+
+
+
+
+/////////////////////////////////////////////////////////
+
+// //Promises and Callbacks
+
+// // ES5
+// aAsync().then(function() {
+// 	returnbAsync();
+// }).then(function() {
+// 	returncAsync();
+// }).done(function() {
+// 	finish();
+// });
+
+// // ES6
+// aAsync().then(() => bAsync()).then(() => cAsync()).done(() => finish);
+
+
+
+
+
+
+
+
+
 
 
 
